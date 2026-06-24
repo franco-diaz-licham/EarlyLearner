0dd9cfb67ae2cb4ee660bb0b2c8614dd4eaaaa59
franco-diaz-licham <franco.diazlicham@hotmail.com>
2026-06-24 17:18:10 +1000

    added household invitation migrations

--- backend/EarlyLearner.Infrastructure/Persistence/Migrations/20260624071524_HouseholdInvitationAddition.Designer.cs
+++ backend/EarlyLearner.Infrastructure/Persistence/Migrations/20260624071524_HouseholdInvitationAddition.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EarlyLearner.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EarlyLearner.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20260624071524_HouseholdInvitationAddition")]
+    partial class HouseholdInvitationAddition
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/EarlyLearner.Infrastructure/Persistence/Migrations/20260624071524_HouseholdInvitationAddition.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EarlyLearner.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class HouseholdInvitationAddition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "given_name",
+                table: "children",
+                newName: "first_name");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_children_household_id_given_name",
+                table: "children",
+                newName: "ix_children_household_id_first_name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "first_name",
+                table: "children",
+                newName: "given_name");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_children_household_id_first_name",
+                table: "children",
+                newName: "ix_children_household_id_given_name");
+        }
+    }
+}
